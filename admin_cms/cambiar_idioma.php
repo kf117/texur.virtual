14719bc6e1375dd3f0f3aeba23299566c549371f
Luciano <fakufum@gmail.com>
2015-09-14 17:40:55 -0300

    Limpiando repo para lucianov88

--- admin_cms/cambiar_idioma.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-$_SESSION["idioma_gestor"]=$_GET["id"];
-echo "<script>window.location = 'index.php'</script>";
--- admin_cms/login/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["usuario_gestor"]);
-unset($_SESSION["idioma_gestor"]);
-header("Location:../index.php");
-?>
